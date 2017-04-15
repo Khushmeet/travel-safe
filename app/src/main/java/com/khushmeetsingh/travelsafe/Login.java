ec13f75189737562dafdd380db6ff5d7885e66e5
Khushmeet <khushmeetsingh1996@gmail.com>
2017-04-16 00:29:52 +0530

    added firebase login

--- app/src/main/java/com/khushmeetsingh/travelsafe/Login.java
+++ app/src/main/java/com/khushmeetsingh/travelsafe/Login.java
@@ -2,19 +2,68 @@ package com.khushmeetsingh.travelsafe;
 
 
 import android.content.Intent;
+import android.support.annotation.IntegerRes;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
+
+    protected EditText phone;
+    protected EditText otp;
+    private FirebaseAuth mFirebaseAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        phone = (EditText)findViewById(R.id.phone);
+        otp = (EditText)findViewById(R.id.otp);
+        mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
-    public void homeActivity(View v){
-        Intent i = new Intent(this, HomeActivity.class);
-        startActivity(i);
+    public void doLogin(View v){
+        String phoneStr = phone.getText().toString();
+        String otp = phone.getText().toString();
+        if (phoneStr.isEmpty() || otp.isEmpty()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+            builder.setMessage("Please enter the details")
+                    .setTitle("Missing")
+                    .setPositiveButton(android.R.string.ok, null);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+        else{
+            mFirebaseAuth.signInWithEmailAndPassword(phoneStr, otp)
+                    .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(Login.this, StartTripActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                                builder.setMessage(task.getException().getMessage())
+                                        .setTitle("Cannot Login")
+                                        .setPositiveButton(android.R.string.ok, null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+        }
     }
 }
--- app/src/main/java/com/khushmeetsingh/travelsafe/MainActivity.java
+++ app/src/main/java/com/khushmeetsingh/travelsafe/MainActivity.java
@@ -5,11 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        if (mFirebaseUser != null) {
+            Intent i = new Intent(this, HomeActivity.class);
+            startActivity(i);
+        }
     }
 
     public void login(View v){
